97a4f2df4d0f615c20b59c1b8e99cddffd77a86c
Mike Coutermarsh <mcoutermarsh@gazelle.com>
2013-06-30 12:12:03 -0400

    blue square

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -5,8 +5,16 @@ class AppDelegate
       delegate: nil,
       cancelButtonTitle: "OK",
       otherButtonTitles: nil)
-    
+
     @alert.show
+
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.applicationFrame)
+    @window.makeKeyAndVisible
+
+    @blue_view = UIView.alloc.initWithFrame(CGRect.new([10,10], [100,100]))
+    @blue_view.backgroundColor = UIColor.blueColor
+
+    @window.addSubview(@blue_view)
 
     puts "Hello from the console!"
     true
